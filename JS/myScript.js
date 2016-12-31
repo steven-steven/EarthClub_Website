aff9c8aa939dbc71cff7e1a1b2d12afe31ec9ff0
Steven <steven.infinity29@gmail.com>
2016-12-30 22:53:15 -0500

    Added the folders of HTML, Javascript, CSS Codes

---
+++ JS/myScript.js
@@ -0,0 +1,37 @@
+function myFunction() {
+   		  document.getElementById('myImage').style.display='inline-block'; 
+
+ }
+	
+function Function2() {
+   		  document.getElementById('Image2').style.display='inline-block'; 
+
+ }
+ function Function3() {
+   		  document.getElementById('Image3').style.display='inline-block'; 
+
+ }
+ function Function4() {
+   		  document.getElementById('Image4').style.display='inline-block'; 
+
+ }
+ function Function5() {
+   		  document.getElementById('Image5').style.display='inline-block'; 
+
+ }
+	
+	
+	
+	
+	
+	
+		
+/*
+		var image = document.getElementById('myImage')
+   		 if (image.style.visibility = 'hidden') {
+   			image.style.visibility='visible';    
+   		  } else {
+ 		       image.src = "hidden";  
+ 		       */
+ 		       
+ 		       
